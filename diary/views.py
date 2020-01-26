4dd40501f0cb7f348ec86a454778d375a12d2c9e
wtkn25 <test@example.com>
2020-01-26 16:24:23 +0900

    chpater8 フォーム画面作成

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,12 @@
 from django.views import generic
 
-# Create your views here.
+from .forms import InquiryForm
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
