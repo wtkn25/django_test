c3ac7352b379af753ec3983bb2d4ca67c97c9c32
wtkn25 <test@example.com>
2020-01-26 20:25:17 +0900

    chapter11の9まで完了

--- diary/views.py
+++ diary/views.py
@@ -37,3 +37,9 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(
             user=self.request.user).order_by('-created_at')
         return diaries
+
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+    # pk_url_kwarg = 'id'  # pkをidに変えたい時の記述
