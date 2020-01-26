418b8f6116dca87377c2b0da8d501c5eb87d1bdb
wtkn25 <test@example.com>
2020-01-26 23:31:09 +0900

    chapter11の10まで完了

--- diary/views.py
+++ diary/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 from .models import Diary
 
 
@@ -43,3 +43,21 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
     # pk_url_kwarg = 'id'  # pkをidに変えたい時の記述
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '日記の作成に失敗しました')
+        return super().form_invalid(form)
